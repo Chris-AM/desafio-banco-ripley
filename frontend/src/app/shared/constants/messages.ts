e1a23610f134555203eb03c3d09fef062fc6e779
chris.am <c.aranguizm@outlook.com>
2021-11-11 01:42:24 -0300

    Global messages added

---
+++ frontend/src/app/shared/constants/messages.ts
@@ -0,0 +1,47 @@
+export const MESSAGES = {
+    HEADER:{
+        BANK: 'Banco Ripley',
+        NEW_TRANSFER: 'Transferir',
+        NEW_RECEIPT: 'Nuevo Destinatario',
+        HISTORY: 'Historial',
+    },
+    SIDE_BAR:{
+        HOME: 'Inicio',
+        MOVEMENTS: 'Movimientos',
+        RECEIPTS: 'Destinatarios',
+    },
+    DASHBOARD:{
+        TITLE: 'Bienvenido',
+        DESCRIPTION: 'Aquí podrás realizar transferencias, agregar destinatarios y ver el historial de movimientos',
+    },
+    TRANSFERENCE:{
+        TITLE: 'Transferir',
+        SEARCH: 'Buscar Destinatario',
+        DETAILS: 'Detalles del destinatario',
+        NAME: 'Nombre',
+        MAIL: 'Correo',
+        BANK: 'Banco',
+        TYPE: 'Tipo de cuenta',
+        MOUNT: 'Monto',
+        ACCEPT: 'Transferir'
+    },
+    NEW_RECEIVER:{
+        TITLE: 'Nuevo Destinatario',
+        NAME: 'Nombre',
+        MAIL: 'Correo',
+        BANK: 'Banco',
+        TYPE: 'Tipo de cuenta',
+        RUT: 'RUT',
+        ACCOUNT: 'Número de cuenta',
+        ACCEPT: 'Crear'
+    },
+    HISTORY:{
+        TITLE: 'Historial',
+        NAMES: 'Destinatarios',
+        RUT: 'RUT',
+        ACCOUNT: 'Número de cuenta',
+        TYPE: 'Tipo de cuenta',
+        MOUNT: 'Monto',
+        DATE: 'Fecha',
+    }
+}
