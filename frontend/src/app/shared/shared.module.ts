0ab29202dc028ddd1a00b43fe97860b02019b4c1
chris.am <c.aranguizm@outlook.com>
2021-11-11 14:46:43 -0300

    module for shared componets added

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  schemas: [
+    NO_ERRORS_SCHEMA
+  ],
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
+})
+export class SharedModule { }
