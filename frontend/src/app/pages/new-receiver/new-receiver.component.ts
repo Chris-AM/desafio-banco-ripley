d9431c85a3f16bd4b53858d2b91a082433f135ce
chris.am <c.aranguizm@outlook.com>
2021-11-12 00:22:10 -0300

    new receiver structure added

--- frontend/src/app/pages/new-receiver/new-receiver.component.ts
+++ frontend/src/app/pages/new-receiver/new-receiver.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as _ from 'lodash';
+import { MESSAGES } from 'src/app/shared/constants/messages';
 @Component({
   selector: 'app-new-receiver',
   templateUrl: './new-receiver.component.html',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewReceiverComponent implements OnInit {
 
+  
+  public title: string = '';
+  public name: string = '';
+  public mail: string = '';
+  public bank: string = '';
+  public type: string = '';
+  public rut: string = '';
+  public account: string = '';
+  public phone: string = '';
+  public accept: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getMessages();
   }
 
+  public getMessages() {
+    this.title = _.get(MESSAGES, 'NEW_RECEIVER.TITLE');
+    this.name = _.get(MESSAGES, 'NEW_RECEIVER.NAME');
+    this.mail = _.get(MESSAGES, 'NEW_RECEIVER.MAIL');
+    this.bank = _.get(MESSAGES, 'NEW_RECEIVER.BANK');
+    this.type = _.get(MESSAGES, 'NEW_RECEIVER.TYPE');
+    this.rut = _.get(MESSAGES, 'NEW_RECEIVER.RUT');
+    this.account = _.get(MESSAGES, 'NEW_RECEIVER.ACCOUNT');
+    this.phone = _.get(MESSAGES, 'NEW_RECEIVER.PHONE');
+    this.accept = _.get(MESSAGES, 'NEW_RECEIVER.ACCEPT');
+  }
+  
 }
