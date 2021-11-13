d59fece9f64774fcc2ea94fcc6b6635e5bc1b8a8
chris.am <c.aranguizm@outlook.com>
2021-11-13 00:12:47 -0300

    model for transfers collection done

---
+++ backend/models/transfers.model.js
@@ -0,0 +1,24 @@
+const {Schema, model} = require('mongoose');
+
+const TransferSchema = Schema({
+    receiver: {
+        type: Schema.Types.ObjectId,
+        ref: 'Receiver',
+        required: true
+    },
+    ammount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+TransferSchema.method('toJSON', function() {
+    const {__v, _id, ...object} = this.toObject();
+    return object;
+})
+
+module.exports = model('Transfer', TransferSchema);
