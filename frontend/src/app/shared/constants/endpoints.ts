4a0abc1dde27d8d559ad3fd9cbfcdcab1f3e37f5
chris.am <c.aranguizm@outlook.com>
2021-11-13 18:53:35 -0300

    adding receipts interface

--- frontend/src/app/shared/constants/endpoints.ts
+++
@@ -1,8 +0,0 @@
-export const ENDPOINTS = {
-    // bank list
-    BANK_LIST: 'https://bast.dev/api/banks.php',
-    BASE_URL: 'http://localhost:3000',
-    //account list
-    ACCOUNT_LIST: '/api/accounts',
-    RECEIVER_LIST: '/api/receivers',
-}
---
+++ frontend/src/app/shared/interfaces/receiptsInterface.ts
@@ -0,0 +1,9 @@
+export interface Receipts{
+    id?: string;
+    name:string;
+    mail:string;
+    phone:string;
+    bank:string;
+    account_type:string;
+    account_number:number;
+} 
