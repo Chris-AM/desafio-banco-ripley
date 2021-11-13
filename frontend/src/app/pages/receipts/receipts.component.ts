78b52fb5195f527307d35a5606e4ed3275dfde9b
chris.am <c.aranguizm@outlook.com>
2021-11-13 05:57:42 -0300

    now showing receivers as cards

--- frontend/src/app/pages/receipts/receipts.component.ts
+++ frontend/src/app/pages/receipts/receipts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
+import { MESSAGES } from 'src/app/shared/constants/messages';
+import { ReceiptsService } from './receipts.service';
 
 @Component({
   selector: 'app-receipts',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceiptsComponent implements OnInit {
 
-  constructor() { }
+
+  //messages
+  public title: string = '';
+  public name: string = '';
+  public mail: string = '';
+  public bank: string = '';
+  public type: string = '';
+  public rut: string = '';
+  public account: string = '';
+  public phone: string = '';
+
+  public receiptList: any[] = [];
+  constructor( private _receiptsService: ReceiptsService ) { }
 
   ngOnInit(): void {
+    this.getMessages();
+    this.getReceipts();
   }
 
+  public getReceipts() {
+    this._receiptsService.getReceipts().subscribe(
+      (data: any) => {
+        console.log('data -->', data);
+        this.receiptList = data.receivers;
+        console.log('this.receiptList -->', this.receiptList);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  public getMessages() {
+    this.title = _.get(MESSAGES, 'RECEIVER.RECEIVERS');
+    this.name = _.get(MESSAGES, 'RECEIVER.NAME');
+    this.mail = _.get(MESSAGES, 'RECEIVER.MAIL');
+    this.bank = _.get(MESSAGES, 'RECEIVER.BANK');
+    this.type = _.get(MESSAGES, 'RECEIVER.TYPE');
+    this.rut = _.get(MESSAGES, 'RECEIVER.RUT');
+    this.account = _.get(MESSAGES, 'RECEIVER.ACCOUNT');
+    this.phone = _.get(MESSAGES, 'RECEIVER.PHONE');
+  }
 }
