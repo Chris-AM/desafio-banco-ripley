57a0634c2249b7e95ed11f26c5b0715807b17bcd
chris.am <c.aranguizm@outlook.com>
2021-11-14 15:56:55 -0300

    getting transactions history and binding to view

--- frontend/src/app/pages/history/history.component.ts
+++ frontend/src/app/pages/history/history.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionsService } from 'src/app/services/transactions.service';
+import { UnsubscribeHelper } from 'src/app/shared/unsubscribe.helper';
+import { MESSAGES } from 'src/app/shared/constants/messages';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss']
+  styleUrls: ['./history.component.scss'],
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent extends UnsubscribeHelper implements OnInit {
+  public title: string = '';
+  public names: string = '';
+  public rut: string = '';
+  public bank: string = '';
+  public account: string = '';
+  public type: string = '';
+  public mount: string = '';
+  public date: string = '';
 
-  constructor() { }
+  public transactions: any[] = [];
 
-  ngOnInit(): void {
+  constructor(private trasactionService: TransactionsService) {
+    super();
   }
 
+  ngOnInit(): void {
+    this.messages();
+    this.getTransactions();
+  }
+
+  messages() {
+    this.title = _.get(MESSAGES, 'HISTORY.TITLE');
+    this.names = _.get(MESSAGES, 'HISTORY.NAMES');
+    this.rut = _.get(MESSAGES, 'HISTORY.RUT');
+    this.bank = _.get(MESSAGES, 'HISTORY.BANK');
+    this.account = _.get(MESSAGES, 'HISTORY.ACCOUNT');
+    this.type = _.get(MESSAGES, 'HISTORY.TYPE');
+    this.mount = _.get(MESSAGES, 'HISTORY.MOUNT');
+    this.date = _.get(MESSAGES, 'HISTORY.DATE');
+  }
+  public getTransactions() {
+    return this.trasactionService.getTransactions().subscribe((data: any) => {
+      this.transactions = data.transfers;
+      console.log(this.transactions);
+    });
+  }
 }
--- frontend/src/app/services/transactions.service.ts
+++ frontend/src/app/services/transactions.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionsService {
 
-  constructor() { }
+  private baseUrl = _.get(environment, 'BASE_URL');
+  private transferUrl = _.get(environment, 'TRANSFER_LIST');
+  private url = this.baseUrl + this.transferUrl;
+
+  constructor( private _httpClient: HttpClient) { }
+
+  getTransactions() {
+    console.log('transfer url ===> ',this.url);
+    return this._httpClient.get(this.url);
+  }
+
+
 }
