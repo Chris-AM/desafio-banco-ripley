e35edbe494a899f378d345c2e7545169b1de7c08
chris.am <c.aranguizm@outlook.com>
2021-11-15 03:38:41 -0300

    refactor: deleting console logs

--- frontend/src/app/services/receipts.service.ts
+++ frontend/src/app/services/receipts.service.ts
@@ -15,13 +15,11 @@ export class ReceiptsService {
   constructor(private _httpClient: HttpClient) {}
 
   getReceipts() {
-    console.log('url -->', this.url);
     return this._httpClient.get(this.url);
   }
 
   getReceiptById(_id: string) {
    const url = this.url + _id;
-   console.log('receiver URL -->', url);
    return this._httpClient.get(url)
   }
 
@@ -31,13 +29,11 @@ export class ReceiptsService {
 
   updateReceipt(id: any, receipt: Receipts) {
     const url = this.url + id;
-    console.log('update url -->', url);
     return this._httpClient.put(url, receipt)
   }
 
   deleteReceipt(id: any) {
     const url = this.url + id;
-    console.log('delete url -->', url);
     return this._httpClient.delete(url)
   }
 }
