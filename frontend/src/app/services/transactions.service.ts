6aeb977aa883e9d4b43566f06e3de3ce6bea1dd9
chris.am <c.aranguizm@outlook.com>
2021-11-15 03:39:27 -0300

    transaction service now allows to make transferences

--- frontend/src/app/services/transactions.service.ts
+++ frontend/src/app/services/transactions.service.ts
@@ -2,22 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as _ from 'lodash';
 import { environment } from 'src/environments/environment';
+import { Transactions } from '../shared/interfaces/transactionsInterface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TransactionsService {
-
   private baseUrl = _.get(environment, 'BASE_URL');
   private transferUrl = _.get(environment, 'TRANSFER_LIST');
   private url = this.baseUrl + this.transferUrl;
 
-  constructor( private _httpClient: HttpClient) { }
+  constructor(private _httpClient: HttpClient) {}
 
   getTransactions() {
-    console.log('transfer url ===> ',this.url);
     return this._httpClient.get(this.url);
   }
 
-
+  createTransaction(transaction: Transactions) {
+    return this._httpClient.post(this.url, transaction);
+  }
 }
